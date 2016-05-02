2a45d6af8b073b217ef562dbf93c302f236c8812
Jacob Westerback (RIT Student) <sjc5283@ad.rit.edu>
2016-05-02 14:59:52 -0400

    Removes Standard Assets & Started Shop scripts

---
+++ Project2/Assets/Scripts/ShopManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopManager : MonoBehaviour {
+
+	//Score
+	public static int AMT_LABELS = 5;
+	public TextMesh[] dmgLabels = new TextMesh[AMT_LABELS];
+	
+	// Use this for initialization
+	void Start () {
+		float[] val = new float[AMT_LABELS];
+		for (int i=0; i < AMT_LABELS; i++) {
+			val[i] = 0;
+			dmgLabels [i].text = "Dmg: "+i;
+		}
+		LoadValues (val);
+		UpdateLabels (val);
+	}
+	
+	void LoadScores (float[] val) {
+		for (int i=0; i < AMT_LABELS; i++) {
+			val [i] = PlayerPrefs.GetFloat ("Score " + i);
+		}
+	}
+	
+	void UpdateLabels (float[] val) {
+		for (int i=0; i < AMT_LABELS; i++) {
+			dmgLabels [i].text = (i+1)+": "+val [i];
+		}
+	}
+}
---
+++ Project2/Assets/Scripts/UpgradeBtn.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpgradeBtn : MonoBehaviour {
+
+	/// <summary>
+	/// Raises the mouse down event.
+	/// When clicked, loads level
+	/// </summary>
+	void OnMouseDown(){		
+
+	}
+}
