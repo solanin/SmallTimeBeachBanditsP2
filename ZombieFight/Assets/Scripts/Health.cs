480b5e0a08ff1b20bf28dc2921c8d79210d734fa
Nameless 14 <Nameless 14>
2016-05-17 11:32:03 -0400

    New Unity Project Files, Health bug fixed

---
+++ ZombieFight/Assets/Scripts/Health.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public Transform healthBar;
+	public double BAR_WIDTH_MAX;
+
+	public double MAX_HEALTH;
+	private double health;
+
+	// Use this for initialization
+	void Start () {
+		reset();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		healthBar.localScale = new Vector3 ((float)(BAR_WIDTH_MAX * (health/MAX_HEALTH)), 0.2f, 1);
+	}
+
+	public void takeDamage(double damage) {
+		health-=damage;
+		if (isDead()) { health = 0.00; }
+        //print((BAR_WIDTH_MAX * (health / MAX_HEALTH)));
+	}
+	
+    public void gainHealth(float gain)
+    {
+        health += gain;
+        if (health > MAX_HEALTH)
+        {
+            health = MAX_HEALTH;
+        }
+    }
+
+	public bool isDead() {
+		return (health <= 0.0f);
+	}
+
+	public void reset() {
+		health = MAX_HEALTH;
+	}
+
+	public double getHealth() {
+		return health;
+	}
+}
